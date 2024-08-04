fbb91789e8450cd20383a75aec1aad3405b4d55c
polloparatodos <kg5zoq+github@protonmail.ch>
2024-08-04 13:56:55 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://pbi5g7pj3m.execute-api.us-east-1.amazonaws.com/api'
+  api_url: 'https://pbi5g7pj3m.execute-api.us-east-1.amazonaws.com'
 };
